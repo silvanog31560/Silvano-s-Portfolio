4a291e90e68d6ab18b6dcdba1a08df74034c6a6e
silvano gonzalez <silvanog31560@gmail.com>
2023-01-04 22:12:22 -0500

    update database settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -5,9 +5,9 @@ ALLOWED_HOSTS = ['silvanogonzalez.pythonanywhere.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'os.environ.get("db_username")$os.environ.get("db_name")',
-        'USER': 'os.environ.get("db_username")',
-        'PASSWORD': 'os.environ.get("db_password")',
+        'NAME': 'silvanogonzalez$portfolio',
+        'USER': 'silvanogonzalez',
+        'PASSWORD': os.environ.get("db_password"),
         'HOST': 'silvanogonzalez.mysql.pythonanywhere-services.com',
     }
 }
