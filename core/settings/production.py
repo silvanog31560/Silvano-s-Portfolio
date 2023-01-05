975ac2fd5ec82beab2c38a042faf0fa75ed6de9e
silvano gonzalez <silvanog31560@gmail.com>
2023-01-04 22:00:23 -0500

    update settings for production

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,10 +1,13 @@
 from .base import *
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['silvanogonzalez.pythonanywhere.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'os.environ.get("db_username")$os.environ.get("db_name")',
+        'USER': 'os.environ.get("db_username")',
+        'PASSWORD': 'os.environ.get("db_password")',
+        'HOST': 'silvanogonzalez.mysql.pythonanywhere-services.com',
     }
 }
